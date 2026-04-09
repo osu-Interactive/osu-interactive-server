002814292681cc6858bd0c407146740492f5e172
Ruichimi <thekeykeyloy@gmail.com>
2026-04-09 17:09:03 +0200

    initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+
+export default async function routes(app: FastifyInstance) {
+    app.get('/', async () => {
+        return { hello: 'world' }
+    })
+}
