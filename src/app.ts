7ff50f6052bb0213654bd494ed5c0743673ace19
Ruichimi <thekeykeyloy@gmail.com>
2026-04-12 16:14:05 +0200

    implement db client with drizzle

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,18 @@ import cors from '@fastify/cors'
 
 import osuApiPlugin from './plugins/osu-api'
 
+import { sql } from 'drizzle-orm'
+import DBClient from './db/client'
+const dbClient = DBClient.getInstance()
+const db = dbClient.db
+
 export async function buildApp() {
     const app = Fastify({
         logger: false,
     })
+
+    const dbPing = await db.execute(sql`SELECT 1`)
+    console.log(dbPing)
 
     await app.register(cors, {
         origin: true,
---
+++ src/db/client.ts
@@ -0,0 +1,49 @@
+import { Pool } from 'pg'
+import { drizzle, NodePgDatabase} from 'drizzle-orm/node-postgres'
+
+class DBClient {
+    private static instance: DBClient
+    private readonly pool: Pool
+    public db: NodePgDatabase
+
+    private constructor() {
+        const {
+            DB_HOST,
+            DB_PORT,
+            DB_NAME,
+            DB_USER,
+            DB_PASSWORD,
+        } = process.env
+
+        if (!DB_HOST || !DB_PASSWORD || !DB_NAME || !DB_USER || !DB_PORT) {
+            throw new Error('Missing required database environment variables')
+        }
+
+        this.pool = new Pool({
+            host: DB_HOST,
+            port: Number(DB_PORT),
+            database: DB_NAME,
+            user: DB_USER,
+            password: DB_PASSWORD,
+        })
+
+        this.pool.on('error', (err) => {
+            console.error('Unexpected DB error', err)
+        })
+
+        this.db = drizzle(this.pool)
+    }
+
+    static getInstance(): DBClient {
+        if (!DBClient.instance) {
+            DBClient.instance = new DBClient()
+        }
+        return DBClient.instance
+    }
+
+    async close(): Promise<void> {
+        await this.pool.end()
+    }
+}
+
+export default DBClient
