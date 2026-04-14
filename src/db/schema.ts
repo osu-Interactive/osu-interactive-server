41bdce3e897c1981cc501d150a1fe8f9ec51fd5e
Ruichimi <thekeykeyloy@gmail.com>
2026-04-14 16:56:57 +0200

    create db users table via migrations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,19 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core'
+import { pgTable, serial, text, integer, jsonb, timestamp  } from 'drizzle-orm/pg-core'
 
-export const testTable = pgTable('TestTable', {
+export const users  = pgTable('users', {
     id: serial('id').primaryKey(),
-    text: text('text').notNull(),
+
+    osu_id: integer('osu_id').notNull().unique(),
+    name: text('name').notNull(),
+    avatar_url: text('avatar_url').notNull(),
+    pp: integer('pp').notNull(),
+
+    country: jsonb('country').$type<{
+        code: string
+        name: string
+    }>().notNull(),
+
+    survey_result: jsonb('survey_result'),
+
+    created_at: timestamp('created_at').defaultNow().notNull()
 })
