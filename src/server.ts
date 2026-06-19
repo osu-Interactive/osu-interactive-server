5da30db1a5eb18e2aba9f7b330ca893b166e4039
Ruichimi <thekeykeyloy@gmail.com>
2026-06-19 16:05:22 +0200

    add server running log

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import { buildApp } from './app'
 
 const start = async () => {
     const app = await buildApp()
+    const port = 3000
 
     try {
         await app.listen({ port: 3000, host: '0.0.0.0' })
+        console.log(`Server is running on port ${port}`)
     } catch (err) {
         app.log.error(err)
         process.exit(1)
