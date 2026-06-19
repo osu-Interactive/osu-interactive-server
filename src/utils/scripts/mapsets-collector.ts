ace1add1bd27f1cca3535f72d020ddc8c5e76232
Ruichimi <thekeykeyloy@gmail.com>
2026-06-19 02:02:18 +0200

    remove limiting requests logic from mapsets collector (bottleneck will care)

--- src/utils/scripts/mapsets-collector.ts
+++ src/utils/scripts/mapsets-collector.ts
@@ -2,19 +2,6 @@ import { getMapset } from '@/services/beatmaps.service'
 import { BeatmapsModel } from '@/models/beatmaps.model'
 
 class MapsetsCollector {
-    private readonly requestsLimit: number
-
-    private requestsThisMinute = {
-        count: 0,
-        time: 0,
-    }
-
-    constructor(requestsInMin: number) {
-        this.requestsLimit = requestsInMin
-
-        this.scheduleRequestsReset()
-    }
-
     public async startFetching(
         mapsetModel: BeatmapsModel,
         amountToFetch: number,
@@ -32,24 +19,7 @@ class MapsetsCollector {
         console.log(`✅ Fetched ${fetchedCount} beatmapsets, fetching complete.`)
     }
 
-    private scheduleRequestsReset(): void {
-        setInterval(() => {
-            this.requestsThisMinute.count = 0
-            this.requestsThisMinute.time = 0
-        }, 60000)
-
-        setInterval(() => {
-            this.requestsThisMinute.time++
-        }, 1000)
-    }
-
     private async fetchBeatmapset(mapsetModel: BeatmapsModel, id: number): Promise<void> {
-        if (this.requestsThisMinute.count >= this.requestsLimit) {
-            await this.waitForRateLimitReset()
-        }
-
-        this.requestsThisMinute.count++
-
         const res = await getMapset(mapsetModel, id, { raw: true })
 
         console.log(
@@ -58,14 +28,6 @@ class MapsetsCollector {
             res ? [res.title, res.artist, res.creator, res.status].join(' | ') : res,
         )
     }
-
-    private async waitForRateLimitReset(): Promise<void> {
-        const waitMs = 60000 - this.requestsThisMinute.time * 1000 + 200
-
-        console.log(`⌛ Waiting for rate limit reset in ${waitMs} ms...`)
-
-        await new Promise<void>((resolve) => setTimeout(resolve, waitMs))
-    }
 }
 
-export default new MapsetsCollector(250)
+export default new MapsetsCollector()
