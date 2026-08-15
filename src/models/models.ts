8837f23fad21c520decc5ff455edad415cba3ac9
Ruichimi <thekeykeyloy@gmail.com>
2026-08-16 01:05:12 +0200

    remove models types

--- src/models/models.ts
+++ src/models/models.ts
@@ -6,6 +6,8 @@ import { calculatedBeatmapsModel } from '@/models/calculated-beatmaps.model'
 import { surveyModel } from '@/models/survey.model'
 import { tagsModel } from '@/models/tags.model'
 
+export type Models = ReturnType<typeof buildModels>
+
 export const modelFactories = {
     user: userModel,
     quests: questsModel,
--- src/types/models.types.ts
+++
@@ -1,3 +0,0 @@
-import type { buildModels } from '@/models/models'
-
-export type Models = ReturnType<typeof buildModels>
