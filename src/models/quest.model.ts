c8c4bd285453d657b6e16b130bc3a4ecfcf1b625
Ruichimi <thekeykeyloy@gmail.com>
2026-09-26 16:30:10 +0200

    fix error due to readonly categories type

--- src/models/quest.model.ts
+++ src/models/quest.model.ts
@@ -6,13 +6,12 @@ import type { Skillset } from '@/config/seeds/skillsets-seed'
 
 export type QuestModelFactory = typeof questsModel
 export type QuestModel = ReturnType<QuestModelFactory>
-export type BeatmapSkillset = Awaited<ReturnType<QuestModel['getRandomBeatmapSkillsets']>>[number]
 
 export const questsModel = (db: DBExecutor) => ({
-    setQuestsCategories(categories: QuestCategory[]) {
+    setQuestsCategories(categories: readonly QuestCategory[]) {
         return db
             .insert(questCategories)
-            .values(categories)
+            .values([...categories])
             .onConflictDoUpdate({
                 target: questCategories.code,
                 set: {
